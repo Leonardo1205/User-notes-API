5ad58b811919aa58467558e1f349b141126e3ab6
Leonardo1205 <leonardoreitz0@gmail.com>
2024-07-09 19:17:27 -0300

    registered password validation

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,18 +1,24 @@
-const knex = require("../database/knex")
-const AppError = require("../utils/AppError")
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
 
 class SessionsController {
     async create(request, response){
         const { email, password} = request.body;
 
-        const user = await knex("users").where({ email }).first()
+        const user = await knex("users").where({ email }).first();
 
         if(!user) {
-            throw new AppError("E-mail e/ou senha incorreta", 401)
+            throw new AppError("E-mail e/ou senha incorreta", 401);
         }
 
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched) {
+            throw new AppError("E-mail e/ou senha incorreta", 401);
+        }
         return response.json(user);
     }
 }
 
-module.exports = SessionsController
+module.exports = SessionsController;
